9b38164ee7673464bdbe9b50168f4b817373cc65
Egor Klementev <egorklementeff@yandex.ru>
2020-02-25 00:36:16 +0300

    BUILDER USAGE
    Problem
    Builder class should be used
    
    Solution
    We have used it
    
    Note
    -

--- src/SearchEngine.java
+++ src/SearchEngine.java
@@ -1,5 +1,17 @@
 public class SearchEngine {
+
     public SearchPage search(String request) {
-        return null;
+
+        SearchPageBuilder builder = new SearchPageBuilder();
+
+        builder.addWebsite(new Website("url1", "title1", 228.0));
+        builder.addWebsite(new Website("url2", "title2", 322.0));
+        builder.addWebsite(new Website("url3", "title3", 123.0));
+        builder.addWebsite(new Website("url4", "title4", 456.0));
+        builder.sortByRank(false);
+        builder.hideWebsitesWithSmallKeywords(10);
+        
+        return builder.getResult();
+
     }
 }
