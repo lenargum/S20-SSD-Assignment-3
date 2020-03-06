cbd836f7d3c2b2e424b26ada5b86277450de3b16
Kamil <kamileshik2700@gmail.com>
2020-03-06 22:29:04 +0300

    update with getter and setter

--- src/project11/Admin.java
+++ src/project11/Admin.java
@@ -5,6 +5,14 @@ import java.util.List;
 public class Admin extends User {
     List<Website> hostedWebsites;
 
+    public List<Website> getHostedWebsites() {
+        return hostedWebsites;
+    }
+
+    public void setHostedWebsites(List<Website> hostedWebsites) {
+        this.hostedWebsites = hostedWebsites;
+    }
+
     public void addWebsite(Website website) {
         hostedWebsites.add(website);
     }
