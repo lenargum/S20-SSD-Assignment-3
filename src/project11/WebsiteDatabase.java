67692f3515814dfaa1bff5cec31ac592de83a86d
lenargum <lenargamgam@gmail.com>
2020-03-06 21:53:19 +0300

    WebsiteDatabaseSingleton

--- src/project11/WebsiteDatabase.java
+++ src/project11/WebsiteDatabase.java
@@ -2,14 +2,30 @@ package project11;
 
 import java.util.List;
 
-public class WebsiteDatabase {
-    public List<Website> storedWebsites;
+class WebsiteDatabaseSingleton {
+    private static WebsiteDatabaseSingleton obj;
+
+    private WebsiteDatabaseSingleton(){}
+
+    public static WebsiteDatabaseSingleton getInstance(){
+        if(obj==null){
+            obj = new WebsiteDatabaseSingleton(); }
+        return obj;
+    }
 
     public void storeWebsite(Website website) {
 
     }
 
     public void removeWebsite(Website website) {
+
+    }
+
+    public Website getWebsite(Website website){
+        return null; // todo
+    }
+
+    public void updateWebsite(Website website){
 
     }
 }
